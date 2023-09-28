f5948638a18ab99afbb879a3b074d4535d21cdd0
Farabi-dev473 <al.farabi.dev@gmail.com>
2023-09-29 03:26:26 +0600

    feat: add cookie & add blocking feature

--- adBlocker.mjs
+++ adBlocker.mjs
@@ -2,24 +2,47 @@ import puppeteer from "puppeteer";
 import { PuppeteerBlocker } from "@cliqz/adblocker-puppeteer";
 import fetch from "cross-fetch";
 
-const browser = await puppeteer.launch({ headless: false });
-const page = await browser.newPage()
-const blocker = await PuppeteerBlocker.fromLists(fetch, [
-    'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt',
-    'https://easylist.to/easylist/easylist.txt'
-])
-await blocker.enableBlockingInPage(page)
+class WebScraper {
+  constructor() {
+    this.browser = null;
+    this.page = null;
+  }
 
-await page.goto('https://www.onetrust.com/products/cookie-consent/', 
-    {waitUntil: "networkidle0"}
-)
+  async initialize() {
+    this.browser = await puppeteer.launch({ headless: false });
+    this.page = await this.browser.newPage();
 
-// const acceptBtn = await  page.waitForSelector('#onetrust-accept-btn-handler', {timeout: 1000}).catch(() => {console.log("consent btn not found")})
-// if(acceptBtn){
-//     await acceptBtn.click()
-// }
+    const blocker = await PuppeteerBlocker.fromLists(fetch, [
+      'https://secure.fanboy.co.nz/fanboy-cookiemonster.txt',
+      'https://easylist.to/easylist/easylist.txt'
+    ]);
+    await blocker.enableBlockingInPage(this.page);
+  }
 
-await page.screenshot({path: 'data/consent.png'})
+  async scrapeWebsite(url) {
+    await this.page.goto(url, { waitUntil: "networkidle0" });
+    await this.page.screenshot({ path: 'data/consent.png' });
+  }
 
-await page.close()
-await browser.close()
+  async close() {
+    if (this.page) {
+      await this.page.close();
+    }
+    if (this.browser) {
+      await this.browser.close();
+    }
+  }
+}
+
+(async () => {
+  const scraper = new WebScraper();
+
+  try {
+    await scraper.initialize();
+    await scraper.scrapeWebsite('https://www.onetrust.com/products/cookie-consent/');
+  } catch (error) {
+    console.error("An error occurred:", error);
+  } finally {
+    await scraper.close();
+  }
+})();
